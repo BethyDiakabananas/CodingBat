ceec9498fb783d2ed9dadfce53ba3b855bc76a21
BethyDiakabananas <bethyd123@gmail.com>
2015-08-02 20:00:00 -0600

    new solution!

---
+++ Java/Warmup1/Or35.java
@@ -0,0 +1,16 @@
+package diakabanab.codingbat.warmup1;
+
+public class Or35 {
+	/**
+	 * 
+	 * Return true if the given non-negative number is a multiple of 3 or a
+	 * multiple of 5. Use the % "mod" operator
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public boolean or35(int n) {
+		return (n % 3 == 0) || (n % 5 == 0);
+	}
+
+}
