7686d8fa2da798f62d498851f6b1c678dbf2da40
BethyDiakabananas <bethyd123@gmail.com>
2016-05-26 14:00:00 +0100

    adding startHi

---
+++ Java/Warmup1/StartHi.java
@@ -0,0 +1,10 @@
+package diakabanab.codingbat.warmup1;
+
+public class StartHi {
+	public boolean startHi(String str) {
+	    if (str.length() < 2) {
+	      return false;
+	    }
+	    return str.substring(0, 2).equals("hi");
+	}
+}
