f30a49f44ec5deab95c2c659c00c8515b08a3d70
BethyDiakabananas <bethyd123@gmail.com>
2016-08-04 22:56:32 -0400

    backaround solution

---
+++ Java/Warmup1/BackAround.java
@@ -0,0 +1,20 @@
+package diakabanab.codingbat.warmup1;
+
+/**
+ * Given a string, take the last char and return a new string with the last char
+ * added at the front and back, so "cat" yields "tcatt". The original string
+ * will be length 1 or more.
+ * 
+ * backAround("cat") → "tcatt" 
+ * backAround("Hello") → "oHelloo" 
+ * backAround("a") → "aaa"
+ * 
+ * @author diakabanab
+ *
+ */
+public class BackAround {
+	public String backAround(String str) {
+		String tail = str.substring(str.length() - 1);
+		return tail + str + tail;
+	}
+}
